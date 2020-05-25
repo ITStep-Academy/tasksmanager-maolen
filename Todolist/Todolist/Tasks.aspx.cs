f73775f8b1ec51c05baf70281c28c58e4360af0e
Kamila <kamila_zh@outlook.com>
2020-05-25 19:31:30 +0600

    Добавила разметку html

---
+++ Todolist/Todolist/Tasks.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Todolist
+{
+    public partial class Tasks : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Request.Cookies.Get("");
+            //Response.Cookies.Set();
+            //Session["data"] = 10;
+        }
+    }
+}
